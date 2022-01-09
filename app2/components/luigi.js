2d2697d0d6ee569aef47887a311bf992089e833a
sergei-clarifai <68540853+sergei-clarifai@users.noreply.github.com>
2022-01-09 17:12:23 +0200

    Removed slashes in config

--- app2/components/luigi.js
+++ app2/components/luigi.js
@@ -13,7 +13,7 @@ const Luigi = () => {
       <h1 className={styles.title}>G'day! I'm Luigi, a microfrontend...</h1>
       <span>
         I'm hosted at
-        <a target="_blank" href="https://mf-app1-theta.vercel.app/">
+        <a target="_blank" href="https://mf-app2-blond.vercel.app/">
           https://mf-app2-blond.vercel.app
         </a>
       </span>
