721d1c113ffa5443702d0b11bc44e800a3bafbda
sergei-clarifai <68540853+sergei-clarifai@users.noreply.github.com>
2022-01-09 16:51:49 +0200

    updated links

--- shell/pages/_document.js
+++ shell/pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <script src="https://mf-app1.vercel.app/_next/static/runtime/app1RemoteEntry.js" />
-        <script src="https://mf-app2.vercel.app/_next/static/runtime/app2RemoteEntry.js" />
+        <script src="https://mf-app1-theta.vercel.app/_next/static/runtime/app1RemoteEntry.js" />
+        <script src="https://mf-app2-blond.vercel.app/_next/static/runtime/app2RemoteEntry.js" />
         <Head />
         <body>
           <Main />
